cf5a23a89d7efcd720053b5a4ee21ceb4101a443
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2024-07-27 21:30:59 +0300

    added route handling in the app, layouts and outlets

--- src/modules/shared/components/MasterLayout/MasterLayout.tsx
+++ src/modules/shared/components/MasterLayout/MasterLayout.tsx
@@ -1,5 +1,23 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+import Sidebar from '../Sidebar/Sidebar';
+
 const MasterLayout = () => {
-  return <div>MasterLayout</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3 bg-primary h-100">
+          <Sidebar />
+        </div>
+        <div className="col-9">
+          <Navbar />
+          <div className="bg-warning">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MasterLayout;
--- src/modules/shared/components/Navbar/Navbar.tsx
+++ src/modules/shared/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className="bg-secondary">Navbar</div>;
 };
 
 export default Navbar;
