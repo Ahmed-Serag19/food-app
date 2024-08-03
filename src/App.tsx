5aa5222a4d322eaedca968b0caa1dc74294e59f1
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2024-08-03 16:16:24 +0300

    Added protected routing and installed jwt token and decoded user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,62 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import AuthLayout from "./modules/shared/components/AuthLayout/AuthLayout";
-import Login from "./modules/auth/components/Login/Login";
-import Register from "./modules/auth/components/Register/Register";
-import ChangePassword from "./modules/auth/components/ChangePassword/ChangePassword";
-import ForgotPassword from "./modules/auth/components/ForgotPassword/ForgotPassword";
-import ResetPassword from "./modules/auth/components/ResetPassword/ResetPassword";
-import MasterLayout from "./modules/shared/components/MasterLayout/MasterLayout";
-import Home from "./modules/home/components/Home/Home";
-import RecipesList from "./modules/recipes/components/RecipesList/RecipesList";
-import CategoriesList from "./modules/categories/components/CategoriesList/CategoriesList";
-import UsersList from "./modules/users/components/UsersList/UsersList";
-import NotFound from "./modules/shared/components/NotFound/NotFound";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import './App.css';
+import AuthLayout from './modules/shared/components/AuthLayout/AuthLayout';
+import Login from './modules/auth/components/Login/Login';
+import Register from './modules/auth/components/Register/Register';
+import ForgotPassword from './modules/auth/components/ForgotPassword/ForgotPassword';
+import ResetPassword from './modules/auth/components/ResetPassword/ResetPassword';
+import MasterLayout from './modules/shared/components/MasterLayout/MasterLayout';
+import Home from './modules/home/components/Home/Home';
+import RecipesList from './modules/recipes/components/RecipesList/RecipesList';
+import CategoriesList from './modules/categories/components/CategoriesList/CategoriesList';
+import UsersList from './modules/users/components/UsersList/UsersList';
+import NotFound from './modules/shared/components/NotFound/NotFound';
+import ProtectedRoute from './modules/shared/components/ProtectedRoute/ProtectedRoute';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [authToken, setAuthToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('authToken');
+    setAuthToken(token);
+  }, []);
+
   const routes = createBrowserRouter([
     {
-      path: "",
+      path: '/',
       element: <AuthLayout />,
       errorElement: <NotFound />,
       children: [
         { index: true, element: <Login /> },
-        { path: "login", element: <Login /> },
-        { path: "register", element: <Register /> },
-        { path: "change-password", element: <ChangePassword /> },
-        { path: "forgot-password", element: <ForgotPassword /> },
-        { path: "reset-password", element: <ResetPassword /> },
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> },
+        { path: 'forgot-password', element: <ForgotPassword /> },
+        { path: 'reset-password', element: <ResetPassword /> },
       ],
     },
     {
-      path: "dashboard",
-      element: <MasterLayout />,
+      path: 'dashboard',
+      element: (
+        <ProtectedRoute authToken={authToken}>
+          <MasterLayout />
+        </ProtectedRoute>
+      ),
       errorElement: <NotFound />,
       children: [
         { index: true, element: <Home /> },
-        { path: "home", element: <Home /> },
-        { path: "register", element: <Register /> },
-        { path: "recipes-list", element: <RecipesList /> },
-        { path: "categories-list", element: <CategoriesList /> },
-        { path: "users-list", element: <UsersList /> },
+        { path: 'home', element: <Home /> },
+        { path: 'recipes-list', element: <RecipesList /> },
+        { path: 'categories-list', element: <CategoriesList /> },
+        { path: 'users-list', element: <UsersList /> },
       ],
     },
   ]);
-  return (
-    <>
-      <RouterProvider router={routes} />
-    </>
-  );
+
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
--- src/modules/shared/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/modules/shared/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,20 @@
-const ProtectedRoute = ({ children, authToken }) => {
-  return <div>ProtectedRoute</div>;
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+type ProtectedRouteProps = {
+  children: React.ReactNode;
+  authToken: string | null;
+};
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  children,
+  authToken,
+}) => {
+  if (authToken) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to="/login" />;
+  }
 };
 
 export default ProtectedRoute;
